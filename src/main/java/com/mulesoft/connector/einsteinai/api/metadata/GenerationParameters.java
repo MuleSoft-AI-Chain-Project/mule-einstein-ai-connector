dc47a73136d7f00322ddf1be29f5b2bd1c429f15
Amir Khan <amir.khan@salesforce.com>
2025-03-12 16:14:08 +0100

    Bug fix unrecognised field
    Trust Layer has a new field in the response which wasn't captured nor ignored.
    
    It resulted in the error:
    Unrecognized field "annotations" (class com.mulesoft.connector.einsteinai.api.metadata.GenerationParameters), not marked as ignorable (5 known properties: "finish_reason", "index", "finsh_reason", "refusal", "logprobs"])
     at [Source: REDACTED (`StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION` disabled); line: 1, column: 560] (through reference chain: com.mulesoft.connector.einsteinai.internal.modelsapi.dto.EinsteinGenerationResponseDTO["generation"]->com.mulesoft.connector.einsteinai.internal.modelsapi.dto.GenerationDTO["parameters"]->com.mulesoft.connector.einsteinai.api.metadata.GenerationParameters["annotations"])
    
    This is the bug fix.

--- src/main/java/com/mulesoft/connector/einsteinai/api/metadata/GenerationParameters.java
+++ src/main/java/com/mulesoft/connector/einsteinai/api/metadata/GenerationParameters.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import java.beans.ConstructorProperties;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
@@ -14,13 +15,15 @@ public class GenerationParameters implements Serializable {
   private final String refusal;
   private final int index;
   private final String logprobs;
+  private final List<String> annotations;
 
-  @ConstructorProperties({"finshReason", "refusal", "index", "logprobs"})
-  public GenerationParameters(String finishReason, String refusal, int index, String logprobs) {
+  @ConstructorProperties({"finshReason", "refusal", "index", "logprobs", "annotations"})
+  public GenerationParameters(String finishReason, String refusal, int index, String logprobs, List<String> annotations) {
     this.finishReason = finishReason;
     this.refusal = refusal;
     this.index = index;
     this.logprobs = logprobs;
+    this.annotations = annotations; // Initialize the annotations field as List<String>
   }
 
   public String getFinishReason() {
@@ -39,6 +42,10 @@ public class GenerationParameters implements Serializable {
     return logprobs;
   }
 
+  public List<String> getAnnotations() { // Getter for annotations
+    return annotations;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o)
@@ -47,11 +54,13 @@ public class GenerationParameters implements Serializable {
       return false;
     GenerationParameters that = (GenerationParameters) o;
     return getIndex() == that.getIndex() && Objects.equals(getFinishReason(), that.getFinishReason())
-        && Objects.equals(getRefusal(), that.getRefusal()) && Objects.equals(getLogprobs(), that.getLogprobs());
+        && Objects.equals(getRefusal(), that.getRefusal()) && Objects.equals(getLogprobs(), that.getLogprobs())
+        && Objects.equals(getAnnotations(), that.getAnnotations()); // Include annotations in equals check
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(getFinishReason(), getRefusal(), getIndex(), getLogprobs());
+    return Objects.hash(getFinishReason(), getRefusal(), getIndex(), getLogprobs(), getAnnotations()); // Include annotations in
+                                                                                                       // hashCode
   }
 }
