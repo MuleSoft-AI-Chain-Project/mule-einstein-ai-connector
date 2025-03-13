b42671132be927799d0c01d50f50ac501fdacf08
ayushagarwal <ayushagarwal@salesforce.com>
2025-03-13 22:51:24 +0530

    Add formatting

--- src/main/java/com/mulesoft/connector/einsteinai/internal/modelsapi/helpers/ObjectMapperSingleton.java
+++ src/main/java/com/mulesoft/connector/einsteinai/internal/modelsapi/helpers/ObjectMapperSingleton.java
@@ -5,14 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ObjectMapperSingleton {
 
-    public static final ObjectMapper INSTANCE = createInstance();
+  public static final ObjectMapper INSTANCE = createInstance();
 
-    // Private constructor to prevent instantiation
-    private ObjectMapperSingleton() {}
+  // Private constructor to prevent instantiation
+  private ObjectMapperSingleton() {}
 
-    private static ObjectMapper createInstance() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return objectMapper;
-    }
+  private static ObjectMapper createInstance() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    return objectMapper;
+  }
 }
