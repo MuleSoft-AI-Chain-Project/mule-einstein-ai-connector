d4aa4e68ff3de22b487d6cf53d44bac18c4e1cca
ayush-salesforce <162944854+ayush-salesforce@users.noreply.github.com>
2025-03-18 13:57:48 +0530

    Feature/update connection validation message (#41)
    * Change connection validation error message
    
    * Added error type parent

--- src/main/java/com/mulesoft/connector/einsteinai/internal/error/EinsteinErrorType.java
+++ src/main/java/com/mulesoft/connector/einsteinai/internal/error/EinsteinErrorType.java
@@ -1,7 +1,24 @@
 package com.mulesoft.connector.einsteinai.internal.error;
 
 import org.mule.runtime.extension.api.error.ErrorTypeDefinition;
+import org.mule.runtime.extension.api.error.MuleErrors;
+
+import java.util.Optional;
 
 public enum EinsteinErrorType implements ErrorTypeDefinition<EinsteinErrorType> {
-  CHAT_FAILURE, EMBEDDING_OPERATIONS_FAILURE, RAG_FAILURE, TOOLS_OPERATION_FAILURE, MODELS_API_ERROR, INVALID_CONNECTION
+
+  CHAT_FAILURE(MuleErrors.ANY), EMBEDDING_OPERATIONS_FAILURE(MuleErrors.ANY), RAG_FAILURE(
+      MuleErrors.ANY), TOOLS_OPERATION_FAILURE(
+          MuleErrors.ANY), MODELS_API_ERROR(MuleErrors.ANY), INVALID_CONNECTION(MuleErrors.CONNECTIVITY);
+
+  private final ErrorTypeDefinition<? extends Enum<?>> parent;
+
+  @Override
+  public Optional<ErrorTypeDefinition<? extends Enum<?>>> getParent() {
+    return Optional.of(parent);
+  }
+
+  EinsteinErrorType(ErrorTypeDefinition<? extends Enum<?>> parent) {
+    this.parent = parent;
+  }
 }
