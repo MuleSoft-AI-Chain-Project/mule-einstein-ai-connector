a7ff4a9dd17e6e84f2028968cceaa382a5ccf9eb
Guillermo Rodríguez González <guillerguezgonz@gmail.com>
2025-08-02 22:27:26 +0200

    Changes: Added mock test

--- src/test/java/com/mulesoft/connector/einsteinai/internal/operation/EinsteinGenerationOperationsTest.java
+++ src/test/java/com/mulesoft/connector/einsteinai/internal/operation/EinsteinGenerationOperationsTest.java
@@ -1,10 +1,14 @@
 package com.mulesoft.connector.einsteinai.internal.operation;
 
+import com.mulesoft.connector.einsteinai.api.metadata.EinsteinPromptTemplateGenerationsResponseAttributes;
 import com.mulesoft.connector.einsteinai.api.metadata.EinsteinResponseAttributes;
 import com.mulesoft.connector.einsteinai.api.metadata.ResponseParameters;
 import com.mulesoft.connector.einsteinai.internal.connection.EinsteinConnection;
 import com.mulesoft.connector.einsteinai.internal.modelsapi.helpers.RequestHelper;
+import com.mulesoft.connector.einsteinai.internal.modelsapi.models.EinsteinLlmAdditionalConfigInputRepresentation;
 import com.mulesoft.connector.einsteinai.internal.modelsapi.models.ParamsModelDetails;
+import com.mulesoft.connector.einsteinai.internal.modelsapi.models.PromptParamsDetails;
+import com.mulesoft.connector.einsteinai.internal.modelsapi.models.WrappedValue;
 import com.mulesoft.connector.einsteinai.internal.params.ReadTimeoutParams;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -18,9 +22,12 @@ import org.mule.runtime.extension.api.exception.ModuleException;
 import org.mule.runtime.extension.api.runtime.process.CompletionCallback;
 
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static com.mulesoft.connector.einsteinai.internal.error.EinsteinErrorType.CHAT_FAILURE;
+import static com.mulesoft.connector.einsteinai.internal.error.EinsteinErrorType.PROMPT_TEMPLATE_GENERATIONS;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.argThat;
@@ -48,6 +55,9 @@ class EinsteinGenerationOperationsTest {
 
   @Mock
   private CompletionCallback<InputStream, ResponseParameters> callbackMock2;
+
+  @Mock
+  private CompletionCallback<InputStream, EinsteinPromptTemplateGenerationsResponseAttributes> callbackMock3;
 
   private ReadTimeoutParams readTimeout;
 
@@ -119,6 +129,33 @@ class EinsteinGenerationOperationsTest {
     verify(callbackMock2).error(argThat(exception -> exception instanceof ModuleException &&
         exception.getMessage().equals("Error while generating the chat from messages Test Messages") &&
         ((ModuleException) exception).getType() == CHAT_FAILURE));
+  }
+
+  @Test
+  void testPromptTemplateFailure() {
+    String promptTemplateDevName = "einstein_gpt__summarizeAccountDefault";
+    PromptParamsDetails promptParamsDetails = new PromptParamsDetails();
+    EinsteinLlmAdditionalConfigInputRepresentation einsteinAdditionConfig = new EinsteinLlmAdditionalConfigInputRepresentation();
+    Map<String, WrappedValue> promptInputParams = new HashMap<>();
+    WrappedValue wrappedValue = new WrappedValue();
+    Map<String, String> value = new HashMap<>();
+    value.put("Id", "");
+    wrappedValue.setValue(value);
+    promptInputParams.put("Input:Account", wrappedValue);
+
+    when(connectionMock.getRequestHelper()).thenReturn(requestHelperMock);
+
+    doThrow(new RuntimeException("Test exception"))
+            .when(requestHelperMock).generateChatFromMessages(
+                    anyString(), any(), eq(readTimeout), any());
+
+    einsteinGenerationOperations.promptTemplateGenerations(connectionMock, promptTemplateDevName, promptInputParams,
+            promptParamsDetails,einsteinAdditionConfig,
+            readTimeout, callbackMock3);
+
+    verify(callbackMock3).error(argThat(exception -> exception instanceof ModuleException &&
+            exception.getMessage().equals("Error while generating prompt template generations") &&
+            ((ModuleException) exception).getType() == PROMPT_TEMPLATE_GENERATIONS));
   }
 }
 
